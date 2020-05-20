a642dd11855880a9174146053333c981d4d77346
chenlizhan <chung1350@hotmail.com>
2020-05-20 21:46:42 +0900

    deploy first version

---
+++ precache-manifest.ee2f37ec1ce9ee988324f4893974094a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc47b97328c07cafe2f",
+    "url": "/css/app.8c1c72ca.css"
+  },
+  {
+    "revision": "0244e151b6f0aba26dc0",
+    "url": "/css/chunk-2dbe5d4c.5a47652e.css"
+  },
+  {
+    "revision": "827a83c72eead0858c1d",
+    "url": "/css/chunk-2ec16bbc.8c444979.css"
+  },
+  {
+    "revision": "98958b4bc0914fd4dff1",
+    "url": "/css/chunk-vendors.10e72359.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "818fb3aa645779eca42a14de83a442ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc47b97328c07cafe2f",
+    "url": "/js/app.b933948d.js"
+  },
+  {
+    "revision": "0244e151b6f0aba26dc0",
+    "url": "/js/chunk-2dbe5d4c.29ecaf34.js"
+  },
+  {
+    "revision": "827a83c72eead0858c1d",
+    "url": "/js/chunk-2ec16bbc.c6cb5ea0.js"
+  },
+  {
+    "revision": "98958b4bc0914fd4dff1",
+    "url": "/js/chunk-vendors.4572b704.js"
+  },
+  {
+    "revision": "96db1f33a064cbf2ae70378ba7f04a17",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
