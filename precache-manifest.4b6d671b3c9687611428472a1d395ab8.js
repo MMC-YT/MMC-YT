b94f4c5ad88d32dbab4ac3bd9ae22c501505ffa4
chenlizhan <chung1350@hotmail.com>
2020-05-23 01:39:47 +0900

    update virtual scroll

---
+++ precache-manifest.4b6d671b3c9687611428472a1d395ab8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4789d0d94cf55d62af19",
+    "url": "/css/chunk-0758a079.4282a794.css"
+  },
+  {
+    "revision": "717e3e3c18aac970f206",
+    "url": "/css/chunk-08c37ec1.8c444979.css"
+  },
+  {
+    "revision": "4ee082d5d91f107ea3cd",
+    "url": "/css/chunk-vendors.6c6d7e4c.css"
+  },
+  {
+    "revision": "d0d7049e85ba04ad5333",
+    "url": "/css/index.e53d6bf0.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1eb2e0ed1a5ceb27aa642db37f130e9f",
+    "url": "/img/animal.jpeg"
+  },
+  {
+    "revision": "d5cb2dffa5828108497d7ba300fbfe40",
+    "url": "/img/background.d5cb2dff.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c0cdc79b56d9c69f9307c89cf4ebad4d",
+    "url": "/img/logo.c0cdc79b.png"
+  },
+  {
+    "revision": "dbbf294376cff5de768bb3c79d923731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4789d0d94cf55d62af19",
+    "url": "/js/chunk-0758a079.e73f8671.js"
+  },
+  {
+    "revision": "717e3e3c18aac970f206",
+    "url": "/js/chunk-08c37ec1.f663b8f9.js"
+  },
+  {
+    "revision": "d06af45e395adf878392",
+    "url": "/js/chunk-e9809e06.ef3fe219.js"
+  },
+  {
+    "revision": "4ee082d5d91f107ea3cd",
+    "url": "/js/chunk-vendors.346a1ce4.js"
+  },
+  {
+    "revision": "d0d7049e85ba04ad5333",
+    "url": "/js/index.3f67c2ec.js"
+  },
+  {
+    "revision": "96db1f33a064cbf2ae70378ba7f04a17",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3afbea10d205eaca4468912498486b53.js"
+  "/precache-manifest.4b6d671b3c9687611428472a1d395ab8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "nookbook"});
