5e2a1033fc1ccf3b1c5701d21b403ff9e8b04335
ChenLiZhan <ChenLiZhan@users.noreply.github.com>
2020-05-30 07:29:35 +0000

    Deploy Commit

---
+++ precache-manifest.cb0fb371744bc01dbb653ff1488578c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b63e95b7b0d68be6b28766e66abbdc",
+    "url": "https://nookstamp.com/index.html"
+  },
+  {
+    "revision": "dfea06908d336605e347",
+    "url": "https://nookstamp.com/static/css/2.203bed75.chunk.css"
+  },
+  {
+    "revision": "bc2b77b9c00f281d284a",
+    "url": "https://nookstamp.com/static/css/main.afff2b2c.chunk.css"
+  },
+  {
+    "revision": "dfea06908d336605e347",
+    "url": "https://nookstamp.com/static/js/2.3e237f88.chunk.js"
+  },
+  {
+    "revision": "e82d2d5ae1faae961eedb89cb7304bc0",
+    "url": "https://nookstamp.com/static/js/2.3e237f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2b77b9c00f281d284a",
+    "url": "https://nookstamp.com/static/js/main.becac0d0.chunk.js"
+  },
+  {
+    "revision": "68cad132554c64fbe494",
+    "url": "https://nookstamp.com/static/js/runtime-main.b6a19f64.js"
+  },
+  {
+    "revision": "d5cb2dffa5828108497d7ba300fbfe40",
+    "url": "https://nookstamp.com/static/media/background.d5cb2dff.png"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "https://nookstamp.com/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "https://nookstamp.com/static/media/element-icons.d2f69a92.woff"
+  }
+]);
